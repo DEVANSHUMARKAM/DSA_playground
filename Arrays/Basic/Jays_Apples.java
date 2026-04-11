f37317a1dca8b686678f4a8766b8c62b59b8d3c3
Devanshu Markam <devanshuasm750@gmail.com>
2026-04-11 21:36:23 +0530

    distributing proper apples to everyone

---
+++ Arrays/Basic/Jays_Apples.java
@@ -0,0 +1,35 @@
+//Given a queue of persons represented by an array of integers
+//, where each person is identified by a specific integer, find the minimum
+//kilograms of apples that need to be distributed, ensuring that each
+// person receives
+////        1 kilogram of apples only once.
+//
+//        Examples:
+//
+//Input: arr[] = [1, 1, 1, 1, 1]
+//Output: 1
+//Explanation: The person identified by '1' appears
+// multiple times but will only receive 1 kilogram of apples once. Therefore,
+// the minimum apples required is 1 kg.
+//        Input: arr[] = [1, 2, 3, 1, 2]
+//Output: 3
+//Explanation: There are three distinct persons in the queue, so
+// 3 kilograms of apples need to be distributed.
+
+    import java.util.HashSet;
+
+public class Jays_Apples {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 1, 2};
+        System.out.println("Minimum apples: " + minimumApple(arr));
+    }
+    public static int minimumApple(int[] arr){
+        HashSet<Integer> distinct = new HashSet<>();
+
+        for(int num : arr){
+            distinct.add(num);
+        }
+
+        return distinct.size();
+    }
+}
